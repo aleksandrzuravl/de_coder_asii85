e4237c4618c109a2da7524afcf6ccc7b7b5b5178
aleksandrzuravl <st129760@student.spbu.ru>
2025-06-02 16:41:35 +0300

    coder.h

--- coder.h
+++ coder.h
@@ -1,1 +1,17 @@
+#ifndef CODER_H
+#define CODER_H
 
+#include "decoder.h"
+#include <string>
+
+class Coder : public Decoder {
+public:
+    std::string getUserInput() const override;
+    std::string encodeString(const std::string& input) const;
+    void displayResult(const std::string& result) const;
+
+private:
+    std::string encodeBlock(uint32_t block, int bytesCount = 4) const;
+};
+
+#endif
