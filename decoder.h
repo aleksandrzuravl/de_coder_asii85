b3990f1e476f8b0265ee15e31d2ac3ef14d196da
aleksandrzuravl <st129760@student.spbu.ru>
2025-06-02 16:39:47 +0300

    decoder.h

--- decoder.h
+++ decoder.h
@@ -1,1 +1,20 @@
+#ifndef DECODER_H
+#define DECODER_H
 
+#include <vector>
+#include <string>
+#include <cstdint>
+
+class Decoder {
+protected:
+    bool testMode = false;
+    
+public:
+    void setTestMode(bool mode);
+    bool isTestMode() const;
+    virtual std::string getUserInput() const;
+    std::vector<uint8_t> decodeData(const std::string& input) const;
+    void displayResult(const std::vector<uint8_t>& result) const;
+};
+
+#endif
